ffc6b3a3f9957be3ad9f730b5c15fe5f7010a440
korayyalcin1903 <korayyalcin1903@gmail.com>
2025-06-15 21:02:35 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿// Bağlantı oluşturma
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://aedqltkd:znYq84Ah2qLfBa0xUq6Zoe8XO38fg8PF@moose.rmq.cloudamqp.com/aedqltkd");
+
+// Bağlantıyı aktifleştirme ve kanal açma
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+// Queue oluşturma
+
+await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false); // COnsumerda da kuyruk publisher daki ile aynı yapılandıma tanımlanmlıdır.
+
+// Queue Mesaj Okuma
+AsyncEventingBasicConsumer consumer = new(channel);
+await channel.BasicConsumeAsync(queue: "example-queue", autoAck: false, consumer:consumer);
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    // Kuyruğa gelen mesajın işlendiği yerdir
+    // e.Body: kuyruğa gelen mesajın içeriğini tutar.
+    // e.Body.Span veya e.Body.ToArray(): Kuyruktaki mesajın byte dizisine erişebiliriz.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://aedqltkd:znYq84Ah2qLfBa0xUq6Zoe8XO38fg8PF@moose.rmq.cloudamqp.com/aedqltkd");
+
+// Bağlantıyı aktifleştirme ve kanal açma
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+// Queue oluşturma
+await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false);
+
+// Queue Mesaj gönderme
+
+// RabbitMQ kuyruğa atacağı mesajları byte türünden kabul eder. Mesalarımızı byte dönüştürmemiz gerekecek.
+for(int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message);
+}
+
+
+Console.Read();
